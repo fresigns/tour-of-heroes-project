25e59060b0a3ca511dbb66c33ea9f742b40b6d6b
fre nzi <64012029+fresigns@users.noreply.github.com>
2023-09-12 00:46:20 +0200

    commit 1
    11/09/23

---
+++ src/appunti.js
@@ -0,0 +1,36 @@
+/* APPUNTI
+
+  npm install -g @angular/cli 
+  ng new tour-of-heroes --directory ./
+
+  -------------------------------------
+  > src
+  > main.ts
+  >app
+  >app.module.ts
+  
+  >assets file aggiuntivi
+  
+  -------------------------------------
+  una volta installato correttamente svuoto src/app/app.component.html
+  -------------------------------------
+  dentro src/app/app.component.html aggiungo
+  <h1>{{title}}</h1>
+  -------------------------------------
+  aggiungo lo style a src/styles.css
+  -------------------------------------
+  ng generate component heroes
+  
+  NB: ng generate crea una nuova directory src/app/heroes/
+  genera i tre file di HeroesComponent insieme a un file di test.
+
+
+
+  
+
+*/
+
+
+/*
+per importare un componente basta usare @Component
+*/
